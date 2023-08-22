dada5ce37929b6e7eaa0e080d7f044b4d2e8a5a3
Benac <1613988200121002@mil>
2023-08-22 01:27:45 -0500

    fix error where headers dissapear in search

--- request-handler.js
+++ request-handler.js
@@ -48,39 +48,39 @@ function requestHandler (req, res) {
     const css = `<style> table, th, td {
     border: 1px solid black; 
     border-collapse: collapse;
-  } 
+    } 
 
-  #myInput {
-    background-image: url('https://img.icons8.com/search'); 
-    background-position: 8px 10px; 
-    background-repeat: no-repeat;
-    background-size: 25px;
-    width: 100%;
-    font-size: 16px; 
-    padding: 12px 20px 12px 40px;
-    border: 1px solid #ddd;
-    margin-bottom: 12px;
-  }
+    #myInput {
+      background-image: url('https://img.icons8.com/search'); 
+      background-position: 8px 10px; 
+      background-repeat: no-repeat;
+      background-size: 25px;
+      width: 100%;
+      font-size: 16px; 
+      padding: 12px 20px 12px 40px;
+      border: 1px solid #ddd;
+      margin-bottom: 12px;
+    }
 
-  #myTable {
-    border-collapse: collapse;
-    width: 100%; 
-    border: 1px solid #ddd; 
-    font-size: 18px; 
-  }
+    #myTable {
+      border-collapse: collapse;
+      width: 100%; 
+      border: 1px solid #ddd; 
+      font-size: 18px; 
+    }
 
-  #myTable th, #myTable td {
-    text-align: left; 
-    padding: 12px; 
-  } 
-  
-  #myTable tr {
-    border-bottom: 1px solid #ddd;
-  } 
-  
-  #myTable tr.header, #myTable tr:hover {
-    background-color: #f1f1f1;
-  } </style>`
+    #myTable th, #myTable td {
+      text-align: left; 
+      padding: 12px; 
+    } 
+    
+    #myTable tr {
+      border-bottom: 1px solid #ddd;
+    } 
+    
+    #myTable tr.header, #myTable tr:hover {
+      background-color: #f1f1f1;
+    } </style>`
 
     // input html
     const search = '<input id="myInput" type="text" onkeyup="myFunction()" placeholder="Search for any value in table...">'
